aed034cf18527b3a98f285a1423dfe9131bf497d
Cesar <50472267+EmilCes@users.noreply.github.com>
2024-11-10 21:21:43 -0600

    First commit

---
+++ src/app/(login)/signIn/page.tsx
@@ -0,0 +1,30 @@
+import LoginForm from "@/components/forms/login.form";
+import Image from "next/image";
+
+const SignIn = () => {
+    return (
+        <div className="min-h-screen grid grid-cols-1 md:grid-cols-2">
+            
+            <div className="flex justify-center items-center bg-white p-8">
+                <div className="w-full max-w-sm">
+                    <h2 className="text-3xl font-semibold text-center mb-6">Log in</h2>
+                    <h3 className="text-alternGray">Welcome back! Please enter your details.</h3>
+                    
+                    <LoginForm />
+
+                </div>
+            </div>
+
+            <div className="hidden md:block relative">
+                <Image 
+                    src="/signInBackground.svg"
+                    alt="Sign In Image"
+                    layout="fill"
+                    objectFit="cover"
+                />
+            </div>
+        </div>
+    )
+}
+
+export default SignIn;
